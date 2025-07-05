3085abe28ebe325b3e88ef46b8546d9c9cd7d7e5
jgkin <himanthagamachchige@gmail.com>
2025-07-05 13:27:00 +0530

    get all product

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
-
+import * as productService from '../services/product.service';
 export const getAllProducts = (req:Request, res:Response) => {
-
+//     ctrl+alt+T to wrap in try catch
+try{
+    const products = productService.getAllProducts();
+    res.status(200).json(products);
+}catch(error) {
+    console.log('Error retrieving products:', error);
+    res.status(500).json({ message: 'Error retrieving products', error });
+  }
 }
 export const saveProduct = (req:Request, res:Response) => {
 
